c111bc7d3d01b257f3f2176281ae91be0e91f4c8
psychophysics pc <adedamola.oni.96@gmail.com>
2021-03-03 16:00:05 +0000

    sync prog updated

--- PupilLabs_TimeSync.py
+++ PupilLabs_TimeSync.py
@@ -1,6 +1,12 @@
 ## pupil labs time sync
 import zmq
 import time 
+from datetime import datetime
+import csv
+import sys
+
+name = sys.argv[1]
+
 
 
 ctx = zmq.Context()
@@ -28,11 +34,22 @@ import msgpack
 
 timestamp = 0 
 
-while True:
-    topic, payload = subscriber.recv_multipart()
-    message = msgpack.loads(payload)
-    #print(f"{topic}: {message}")
-    new_timestamp = message[b'timestamp']
-    if new_timestamp - timestamp >= 1: 
-    	timestamp = new_timestamp
-    	print("timestamp: %.2f, pupil diameter: %.2f" %(timestamp, message[b'diameter']))
+
+
+csvDirectory = "./timeSync_csvs/"
+print(name)
+with open(csvDirectory+name+'.csv', 'a', newline='') as csvFile: 
+	print(name)
+	csvWriter = csv.writer(csvFile, delimiter=',')
+	csvWriter.writerow(['PupilLabsTimeStamp','TrueTime'])
+
+	while True:
+		topic, payload = subscriber.recv_multipart()
+		message = msgpack.loads(payload)                                                              
+		new_timestamp = message[b'timestamp']
+		if new_timestamp - timestamp >= 1: 
+			timestamp = new_timestamp
+			now = datetime.now()
+			current_time = now.strftime("%H:%M:%S.%f")
+			csvWriter.writerow([current_time,timestamp])
+			print("time: %s, pupil labs timestamp: %.2f, pupil diameter: %.2f" %(current_time, timestamp, message[b'diameter']))
